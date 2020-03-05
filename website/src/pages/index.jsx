c7250387a60c72fad5d543a8d0160311085b349e
Kaelig Deloumeau-Prigent <kaelig@users.noreply.github.com>
2020-03-04 22:57:55 -0800

    Test PR review apps (by netlify)

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Design Tokens W3C Community Group</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
