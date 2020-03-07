a6a0315dfefd6372d1a292d6d1cd6d79cd960aa8
Kaelig Deloumeau-Prigent <kaelig@users.noreply.github.com>
2020-03-06 21:35:36 -0800

    Enable MDX support (#3)
    * Enable mdx support
    * Disable warnings for duplicate selectors in the Layout Sass file

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import DefaultLayout from '../components/layouts/DefaultLayout';
+import SEO from '../components/SEO';
 
 const IndexPage = () => (
-  <Layout>
+  <DefaultLayout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </p>
+    <p>
+      <Link to="/principles/">Go to the principles page (in MDX)</Link>
+    </p>
+    <p>
+      <Link to="/test/">Load an MDX test page</Link>
+    </p>
+  </DefaultLayout>
 );
 
 export default IndexPage;
