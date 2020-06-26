762348c45c4915229b6901b16f1980184f7e0422
Kaelig <kaelig.deloumeauprigent@shopify.com>
2020-06-25 20:37:24 -0700

    Use Sass MQ to manage breakpoints

--- website/src/dependencies/index.js
+++ website/src/dependencies/index.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 // These files are automatically imported in .scss files
 module.exports = [
-  path.resolve(__dirname, '_functions.scss'),
   path.resolve(__dirname, '_variables.scss'),
+  path.resolve(__dirname, '_mq.scss'),
+  path.resolve(__dirname, '_functions.scss'),
   path.resolve(__dirname, '_mixins.scss'),
 ];
