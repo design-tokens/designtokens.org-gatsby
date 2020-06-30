9adf32a22218e93c3c1662f6945945fa87a61542
Kaelig <kaelig.deloumeauprigent@shopify.com>
2020-06-30 15:43:09 -0700

    Fix miniCssExtract config

--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -6,13 +6,29 @@
 
 const webpackStyleRules = require('./webpack-style-rules');
 
-exports.onCreateWebpackConfig = ({ actions, loaders }) => {
+exports.onCreateWebpackConfig = ({ actions, loaders, stage, getConfig }) => {
+  const isSSR = stage.includes(`html`);
+
+  if (stage === 'build-javascript') {
+    const config = getConfig();
+    const miniCssExtractPlugin = config.plugins.find(
+      (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin',
+    );
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true;
+    }
+    actions.replaceWebpackConfig(config);
+  }
+
   actions.setWebpackConfig({
     module: {
       rules: [
         {
           test: /\.scss$/,
-          use: [loaders.miniCssExtract(), ...webpackStyleRules],
+          use: [
+            !isSSR && loaders.miniCssExtract({ hmr: false }),
+            ...webpackStyleRules,
+          ].filter(Boolean),
         },
       ],
     },
