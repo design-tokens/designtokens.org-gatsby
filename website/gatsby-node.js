16a3e6b471491bc1d2034cbbe42cb4a5ad190b18
Kaelig Deloumeau-Prigent <kaelig@users.noreply.github.com>
2020-07-03 08:35:33 -0700

    Fix bugs around CSS Modules class naming and sourcemaps (#65)

--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -4,4 +4,16 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions, stage, getConfig }) => {
+  // See https://spectrum.chat/gatsby-js/general/having-issue-related-to-chunk-commons-mini-css-extract-plugin~0ee9c456-a37e-472a-a1a0-cc36f8ae6033?m=MTU3MjYyNDQ5OTAyNQ==
+  if (stage === 'build-javascript') {
+    const config = getConfig();
+    const miniCssExtractPlugin = config.plugins.find(
+      (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin',
+    );
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true;
+    }
+    actions.replaceWebpackConfig(config);
+  }
+};
