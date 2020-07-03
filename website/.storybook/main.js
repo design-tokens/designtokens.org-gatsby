d7c0c62cc621e0584fa7660b3379ce5044bbd663
Kaelig <kaelig.deloumeauprigent@shopify.com>
2020-07-03 16:08:39 -0700

    Enable optional chaining and centralize the babel config

--- website/.storybook/main.js
+++ website/.storybook/main.js
@@ -27,26 +27,6 @@ module.exports = {
     // Since we're using Yarn Workspaces and nesting the packages, we need to include the parent package
     config.module.rules[0].include = require('path').resolve('../..');
 
-    // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-    config.module.rules[0].use[0].loader = require.resolve('babel-loader');
-
-    // use @babel/preset-react for JSX and env (instead of staged presets)
-    config.module.rules[0].use[0].options.presets = [
-      require.resolve('@babel/preset-react'),
-      require.resolve('@babel/preset-env'),
-    ];
-
-    config.module.rules[0].use[0].options.plugins = [
-      // use @babel/plugin-proposal-class-properties for class arrow functions
-      require.resolve('@babel/plugin-proposal-class-properties'),
-
-      // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-      require.resolve('babel-plugin-remove-graphql-queries'),
-
-      // Generate prop tables from prop types
-      require.resolve('babel-plugin-react-docgen'),
-    ];
-
     // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
     config.resolve.mainFields = ['browser', 'module', 'main'];
 
